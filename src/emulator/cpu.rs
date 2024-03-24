e5ea459bd919118a225694ce7606602c0af040d2
Youlixx <combey.theo@hotmail.com>
2024-03-24 18:04:04 +0100

    Improved LD r,r' op codes

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -66,54 +66,54 @@ const OP_CODE_FUNCTION_TABLE: [fn(&mut Cpu); 256] = [
     Cpu::op_dec_a,         // 0x3D : DEC A
     Cpu::op_ld_a_u8,       // 0x3E : LD A,d8
     Cpu::op_ccf,           // 0x3F : CCF
-    Cpu::op_load_b_b,      // 0x40 : LD B,B
-    Cpu::op_load_b_c,      // 0x41 : LD B,C
-    Cpu::op_load_b_d,      // 0x42 : LD B,D
-    Cpu::op_load_b_e,      // 0x43 : LD B,E
-    Cpu::op_load_b_h,      // 0x44 : LD B,H
-    Cpu::op_load_b_l,      // 0x45 : LD B,L
+    Cpu::op_ld_b_b,        // 0x40 : LD B,B
+    Cpu::op_ld_b_c,        // 0x41 : LD B,C
+    Cpu::op_ld_b_d,        // 0x42 : LD B,D
+    Cpu::op_ld_b_e,        // 0x43 : LD B,E
+    Cpu::op_ld_b_h,        // 0x44 : LD B,H
+    Cpu::op_ld_b_l,        // 0x45 : LD B,L
     Cpu::op_load_b_hl,     // 0x46 : LD B,(HL)
-    Cpu::op_load_b_a,      // 0x47 : LD B,A
-    Cpu::op_load_c_b,      // 0x48 : LD C,B
-    Cpu::op_load_c_c,      // 0x49 : LD C,C
-    Cpu::op_load_c_d,      // 0x4A : LD C,D
-    Cpu::op_load_c_e,      // 0x4B : LD C,E
-    Cpu::op_load_c_h,      // 0x4C : LD C,H
-    Cpu::op_load_c_l,      // 0x4D : LD C,L
+    Cpu::op_ld_b_a,        // 0x47 : LD B,A
+    Cpu::op_ld_c_b,        // 0x48 : LD C,B
+    Cpu::op_ld_c_c,        // 0x49 : LD C,C
+    Cpu::op_ld_c_d,        // 0x4A : LD C,D
+    Cpu::op_ld_c_e,        // 0x4B : LD C,E
+    Cpu::op_ld_c_h,        // 0x4C : LD C,H
+    Cpu::op_ld_c_l,        // 0x4D : LD C,L
     Cpu::op_load_c_hl,     // 0x4E : LD C,(HL)
-    Cpu::op_load_c_a,      // 0x4F : LD C,A
-    Cpu::op_load_d_b,      // 0x50 : LD D,B
-    Cpu::op_load_d_c,      // 0x51 : LD D,C
-    Cpu::op_load_d_d,      // 0x52 : LD D,D
-    Cpu::op_load_d_e,      // 0x53 : LD D,E
-    Cpu::op_load_d_h,      // 0x54 : LD D,H
-    Cpu::op_load_d_l,      // 0x55 : LD D,L
+    Cpu::op_ld_c_a,        // 0x4F : LD C,A
+    Cpu::op_ld_d_b,        // 0x50 : LD D,B
+    Cpu::op_ld_d_c,        // 0x51 : LD D,C
+    Cpu::op_ld_d_d,        // 0x52 : LD D,D
+    Cpu::op_ld_d_e,        // 0x53 : LD D,E
+    Cpu::op_ld_d_h,        // 0x54 : LD D,H
+    Cpu::op_ld_d_l,        // 0x55 : LD D,L
     Cpu::op_load_d_hl,     // 0x56 : LD D,(HL)
-    Cpu::op_load_d_a,      // 0x57 : LD D,A
-    Cpu::op_load_e_b,      // 0x58 : LD E,B
-    Cpu::op_load_e_c,      // 0x59 : LD E,C
-    Cpu::op_load_e_d,      // 0x5A : LD E,D
-    Cpu::op_load_e_e,      // 0x5B : LD E,E
-    Cpu::op_load_e_h,      // 0x5C : LD E,H
-    Cpu::op_load_e_l,      // 0x5D : LD E,L
+    Cpu::op_ld_d_a,        // 0x57 : LD D,A
+    Cpu::op_ld_e_b,        // 0x58 : LD E,B
+    Cpu::op_ld_e_c,        // 0x59 : LD E,C
+    Cpu::op_ld_e_d,        // 0x5A : LD E,D
+    Cpu::op_ld_e_e,        // 0x5B : LD E,E
+    Cpu::op_ld_e_h,        // 0x5C : LD E,H
+    Cpu::op_ld_e_l,        // 0x5D : LD E,L
     Cpu::op_load_e_hl,     // 0x5E : LD E,(HL)
-    Cpu::op_load_e_a,      // 0x5F : LD E,A
-    Cpu::op_load_h_b,      // 0x60 : LD H,B
-    Cpu::op_load_h_c,      // 0x61 : LD H,C
-    Cpu::op_load_h_d,      // 0x62 : LD H,D
-    Cpu::op_load_h_e,      // 0x63 : LD H,E
-    Cpu::op_load_h_h,      // 0x64 : LD H,H
-    Cpu::op_load_h_l,      // 0x65 : LD H,L
+    Cpu::op_ld_e_a,        // 0x5F : LD E,A
+    Cpu::op_ld_h_b,        // 0x60 : LD H,B
+    Cpu::op_ld_h_c,        // 0x61 : LD H,C
+    Cpu::op_ld_h_d,        // 0x62 : LD H,D
+    Cpu::op_ld_h_e,        // 0x63 : LD H,E
+    Cpu::op_ld_h_h,        // 0x64 : LD H,H
+    Cpu::op_ld_h_l,        // 0x65 : LD H,L
     Cpu::op_load_h_hl,     // 0x66 : LD H,(HL)
-    Cpu::op_load_h_a,      // 0x67 : LD H,A
-    Cpu::op_load_l_b,      // 0x68 : LD L,B
-    Cpu::op_load_l_c,      // 0x69 : LD L,C
-    Cpu::op_load_l_d,      // 0x6A : LD L,D
-    Cpu::op_load_l_e,      // 0x6B : LD L,E
-    Cpu::op_load_l_h,      // 0x6C : LD L,H
-    Cpu::op_load_l_l,      // 0x6D : LD L,L
+    Cpu::op_ld_h_a,        // 0x67 : LD H,A
+    Cpu::op_ld_l_b,        // 0x68 : LD L,B
+    Cpu::op_ld_l_c,        // 0x69 : LD L,C
+    Cpu::op_ld_l_d,        // 0x6A : LD L,D
+    Cpu::op_ld_l_e,        // 0x6B : LD L,E
+    Cpu::op_ld_l_h,        // 0x6C : LD L,H
+    Cpu::op_ld_l_l,        // 0x6D : LD L,L
     Cpu::op_load_l_hl,     // 0x6E : LD L,(HL)
-    Cpu::op_load_l_a,      // 0x6F : LD L,A
+    Cpu::op_ld_l_a,        // 0x6F : LD L,A
     Cpu::op_load_hl_b,     // 0x70 : LD (HL),B
     Cpu::op_load_hl_c,     // 0x71 : LD (HL),C
     Cpu::op_load_hl_d,     // 0x72 : LD (HL),D
@@ -122,14 +122,14 @@ const OP_CODE_FUNCTION_TABLE: [fn(&mut Cpu); 256] = [
     Cpu::op_load_hl_l,     // 0x75 : LD (HL),L
     Cpu::op_halt,          // 0x76 : HALT
     Cpu::op_load_hl_a,     // 0x77 : LD (HL),A
-    Cpu::op_load_a_b,      // 0x78 : LD A,B
-    Cpu::op_load_a_c,      // 0x79 : LD A,C
-    Cpu::op_load_a_d,      // 0x7A : LD A,D
-    Cpu::op_load_a_e,      // 0x7B : LD A,E
-    Cpu::op_load_a_h,      // 0x7C : LD A,H
-    Cpu::op_load_a_l,      // 0x7D : LD A,L
+    Cpu::op_ld_a_b,        // 0x78 : LD A,B
+    Cpu::op_ld_a_c,        // 0x79 : LD A,C
+    Cpu::op_ld_a_d,        // 0x7A : LD A,D
+    Cpu::op_ld_a_e,        // 0x7B : LD A,E
+    Cpu::op_ld_a_h,        // 0x7C : LD A,H
+    Cpu::op_ld_a_l,        // 0x7D : LD A,L
     Cpu::op_load_a_hl,     // 0x7E : LD A,(HL)
-    Cpu::op_load_a_a,      // 0x7F : LD A,A
+    Cpu::op_ld_a_a,        // 0x7F : LD A,A
     Cpu::op_add_a_b,       // 0x80 : ADD A,B
     Cpu::op_add_a_c,       // 0x81 : ADD A,C
     Cpu::op_add_a_d,       // 0x82 : ADD A,D
@@ -435,72 +435,202 @@ impl Cpu {
     }
 }
 
-macro_rules! op_load_r_r {
-    ($x:tt, $y:tt) => {
-        paste! {
-            fn [< op_load_ $x _ $y >] (&mut self) {
-                self.registers. [< register_ $x >] = self.registers. [< register_ $y >];
-            }
-        }
-    };
-}
-
 impl Cpu {
-    op_load_r_r!(b, b);
-    op_load_r_r!(b, c);
-    op_load_r_r!(b, d);
-    op_load_r_r!(b, e);
-    op_load_r_r!(b, h);
-    op_load_r_r!(b, l);
-    op_load_r_r!(b, a);
+    fn op_ld_a_a(&mut self) {
+        self.registers.register_a = self.registers.register_a;
+    }
 
-    op_load_r_r!(c, b);
-    op_load_r_r!(c, c);
-    op_load_r_r!(c, d);
-    op_load_r_r!(c, e);
-    op_load_r_r!(c, h);
-    op_load_r_r!(c, l);
-    op_load_r_r!(c, a);
+    fn op_ld_a_b(&mut self) {
+        self.registers.register_a = self.registers.register_b;
+    }
 
-    op_load_r_r!(d, b);
-    op_load_r_r!(d, c);
-    op_load_r_r!(d, d);
-    op_load_r_r!(d, e);
-    op_load_r_r!(d, h);
-    op_load_r_r!(d, l);
-    op_load_r_r!(d, a);
+    fn op_ld_a_c(&mut self) {
+        self.registers.register_a = self.registers.register_c;
+    }
 
-    op_load_r_r!(e, b);
-    op_load_r_r!(e, c);
-    op_load_r_r!(e, d);
-    op_load_r_r!(e, e);
-    op_load_r_r!(e, h);
-    op_load_r_r!(e, l);
-    op_load_r_r!(e, a);
+    fn op_ld_a_d(&mut self) {
+        self.registers.register_a = self.registers.register_d;
+    }
 
-    op_load_r_r!(h, b);
-    op_load_r_r!(h, c);
-    op_load_r_r!(h, d);
-    op_load_r_r!(h, e);
-    op_load_r_r!(h, h);
-    op_load_r_r!(h, l);
-    op_load_r_r!(h, a);
+    fn op_ld_a_e(&mut self) {
+        self.registers.register_a = self.registers.register_e;
+    }
 
-    op_load_r_r!(l, b);
-    op_load_r_r!(l, c);
-    op_load_r_r!(l, d);
-    op_load_r_r!(l, e);
-    op_load_r_r!(l, h);
-    op_load_r_r!(l, l);
-    op_load_r_r!(l, a);
+    fn op_ld_a_h(&mut self) {
+        self.registers.register_a = self.registers.register_h;
+    }
 
-    op_load_r_r!(a, b);
-    op_load_r_r!(a, c);
-    op_load_r_r!(a, d);
-    op_load_r_r!(a, e);
-    op_load_r_r!(a, h);
-    op_load_r_r!(a, l);
-    op_load_r_r!(a, a);
+    fn op_ld_a_l(&mut self) {
+        self.registers.register_a = self.registers.register_l;
+    }
+
+    fn op_ld_b_a(&mut self) {
+        self.registers.register_b = self.registers.register_a;
+    }
+
+    fn op_ld_b_b(&mut self) {
+        self.registers.register_b = self.registers.register_b;
+    }
+
+    fn op_ld_b_c(&mut self) {
+        self.registers.register_b = self.registers.register_c;
+    }
+
+    fn op_ld_b_d(&mut self) {
+        self.registers.register_b = self.registers.register_d;
+    }
+
+    fn op_ld_b_e(&mut self) {
+        self.registers.register_b = self.registers.register_e;
+    }
+
+    fn op_ld_b_h(&mut self) {
+        self.registers.register_b = self.registers.register_h;
+    }
+
+    fn op_ld_b_l(&mut self) {
+        self.registers.register_b = self.registers.register_l;
+    }
+
+    fn op_ld_c_a(&mut self) {
+        self.registers.register_c = self.registers.register_a;
+    }
+
+    fn op_ld_c_b(&mut self) {
+        self.registers.register_c = self.registers.register_b;
+    }
+
+    fn op_ld_c_c(&mut self) {
+        self.registers.register_c = self.registers.register_c;
+    }
+
+    fn op_ld_c_d(&mut self) {
+        self.registers.register_c = self.registers.register_d;
+    }
+
+    fn op_ld_c_e(&mut self) {
+        self.registers.register_c = self.registers.register_e;
+    }
+
+    fn op_ld_c_h(&mut self) {
+        self.registers.register_c = self.registers.register_h;
+    }
+
+    fn op_ld_c_l(&mut self) {
+        self.registers.register_c = self.registers.register_l;
+    }
+
+    fn op_ld_d_a(&mut self) {
+        self.registers.register_d = self.registers.register_a;
+    }
+
+    fn op_ld_d_b(&mut self) {
+        self.registers.register_d = self.registers.register_b;
+    }
+
+    fn op_ld_d_c(&mut self) {
+        self.registers.register_d = self.registers.register_c;
+    }
+
+    fn op_ld_d_d(&mut self) {
+        self.registers.register_d = self.registers.register_d;
+    }
+
+    fn op_ld_d_e(&mut self) {
+        self.registers.register_d = self.registers.register_e;
+    }
+
+    fn op_ld_d_h(&mut self) {
+        self.registers.register_d = self.registers.register_h;
+    }
+
+    fn op_ld_d_l(&mut self) {
+        self.registers.register_d = self.registers.register_l;
+    }
+
+    fn op_ld_e_a(&mut self) {
+        self.registers.register_e = self.registers.register_a;
+    }
+
+    fn op_ld_e_b(&mut self) {
+        self.registers.register_e = self.registers.register_b;
+    }
+
+    fn op_ld_e_c(&mut self) {
+        self.registers.register_e = self.registers.register_c;
+    }
+
+    fn op_ld_e_d(&mut self) {
+        self.registers.register_e = self.registers.register_d;
+    }
+
+    fn op_ld_e_e(&mut self) {
+        self.registers.register_e = self.registers.register_e;
+    }
+
+    fn op_ld_e_h(&mut self) {
+        self.registers.register_e = self.registers.register_h;
+    }
+
+    fn op_ld_e_l(&mut self) {
+        self.registers.register_e = self.registers.register_l;
+    }
+
+    fn op_ld_h_a(&mut self) {
+        self.registers.register_h = self.registers.register_a;
+    }
+
+    fn op_ld_h_b(&mut self) {
+        self.registers.register_h = self.registers.register_b;
+    }
+
+    fn op_ld_h_c(&mut self) {
+        self.registers.register_h = self.registers.register_c;
+    }
+
+    fn op_ld_h_d(&mut self) {
+        self.registers.register_h = self.registers.register_d;
+    }
+
+    fn op_ld_h_e(&mut self) {
+        self.registers.register_h = self.registers.register_e;
+    }
+
+    fn op_ld_h_h(&mut self) {
+        self.registers.register_h = self.registers.register_h;
+    }
+
+    fn op_ld_h_l(&mut self) {
+        self.registers.register_h = self.registers.register_l;
+    }
+
+    fn op_ld_l_a(&mut self) {
+        self.registers.register_l = self.registers.register_a;
+    }
+
+    fn op_ld_l_b(&mut self) {
+        self.registers.register_l = self.registers.register_b;
+    }
+
+    fn op_ld_l_c(&mut self) {
+        self.registers.register_l = self.registers.register_c;
+    }
+
+    fn op_ld_l_d(&mut self) {
+        self.registers.register_l = self.registers.register_d;
+    }
+
+    fn op_ld_l_e(&mut self) {
+        self.registers.register_l = self.registers.register_e;
+    }
+
+    fn op_ld_l_h(&mut self) {
+        self.registers.register_l = self.registers.register_h;
+    }
+
+    fn op_ld_l_l(&mut self) {
+        self.registers.register_l = self.registers.register_l;
+    }
 }
 
 macro_rules! op_load_hl_r {
