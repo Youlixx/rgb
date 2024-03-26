2242d03fc462147013ab4d55b30cf5b102210c28
Youlixx <combey.theo@hotmail.com>
2024-03-26 20:52:44 +0100

    Added RLC cb prefix op

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -259,6 +259,265 @@ const OP_CODE_FUNCTION_TABLE: [fn(&mut Cpu); 256] = [
     Cpu::op_rst_38h,     // 0xFF : RST 38H
 ];
 
+const CB_CODE_FUNCTION_TABLE: [fn(&mut Cpu); 256] = [
+    Cpu::cb_rlc_b,       // 0x00 : RLC B
+    Cpu::cb_rlc_c,       // 0x01 : RLC C
+    Cpu::cb_rlc_d,       // 0x02 : RLC D
+    Cpu::cb_rlc_e,       // 0x03 : RLC E
+    Cpu::cb_rlc_h,       // 0x04 : RLC H
+    Cpu::cb_rlc_l,       // 0x05 : RLC L
+    Cpu::cb_rlc_hl,      // 0x06 : RLC (HL)
+    Cpu::cb_rlc_a,       // 0x07 : RLC A
+    Cpu::op_placeholder, // 0x08 : RRC B
+    Cpu::op_placeholder, // 0x09 : RRC C
+    Cpu::op_placeholder, // 0x0A : RRC D
+    Cpu::op_placeholder, // 0x0B : RRC E
+    Cpu::op_placeholder, // 0x0C : RRC H
+    Cpu::op_placeholder, // 0x0D : RRC L
+    Cpu::op_placeholder, // 0x0E : RRC (HL)
+    Cpu::op_placeholder, // 0x0F : RRC A
+    Cpu::op_placeholder, // 0x10 : RL B
+    Cpu::op_placeholder, // 0x11 : RL C
+    Cpu::op_placeholder, // 0x12 : RL D
+    Cpu::op_placeholder, // 0x13 : RL E
+    Cpu::op_placeholder, // 0x14 : RL H
+    Cpu::op_placeholder, // 0x15 : RL L
+    Cpu::op_placeholder, // 0x16 : RL (HL)
+    Cpu::op_placeholder, // 0x17 : RL A
+    Cpu::op_placeholder, // 0x18 : RR B
+    Cpu::op_placeholder, // 0x19 : RR C
+    Cpu::op_placeholder, // 0x1A : RR D
+    Cpu::op_placeholder, // 0x1B : RR E
+    Cpu::op_placeholder, // 0x1C : RR H
+    Cpu::op_placeholder, // 0x1D : RR L
+    Cpu::op_placeholder, // 0x1E : RR (HL)
+    Cpu::op_placeholder, // 0x1F : RR A
+    Cpu::op_placeholder, // 0x20 : SLA B
+    Cpu::op_placeholder, // 0x21 : SLA C
+    Cpu::op_placeholder, // 0x22 : SLA D
+    Cpu::op_placeholder, // 0x23 : SLA E
+    Cpu::op_placeholder, // 0x24 : SLA H
+    Cpu::op_placeholder, // 0x25 : SLA L
+    Cpu::op_placeholder, // 0x26 : SLA (HL)
+    Cpu::op_placeholder, // 0x27 : SLA A
+    Cpu::op_placeholder, // 0x28 : SRA B
+    Cpu::op_placeholder, // 0x29 : SRA C
+    Cpu::op_placeholder, // 0x2A : SRA D
+    Cpu::op_placeholder, // 0x2B : SRA E
+    Cpu::op_placeholder, // 0x2C : SRA H
+    Cpu::op_placeholder, // 0x2D : SRA L
+    Cpu::op_placeholder, // 0x2E : SRA (HL)
+    Cpu::op_placeholder, // 0x2F : SRA A
+    Cpu::op_placeholder, // 0x30 : SWAP B
+    Cpu::op_placeholder, // 0x31 : SWAP C
+    Cpu::op_placeholder, // 0x32 : SWAP D
+    Cpu::op_placeholder, // 0x33 : SWAP E
+    Cpu::op_placeholder, // 0x34 : SWAP H
+    Cpu::op_placeholder, // 0x35 : SWAP L
+    Cpu::op_placeholder, // 0x36 : SWAP (HL)
+    Cpu::op_placeholder, // 0x37 : SWAP A
+    Cpu::op_placeholder, // 0x38 : SRL B
+    Cpu::op_placeholder, // 0x39 : SRL C
+    Cpu::op_placeholder, // 0x3A : SRL D
+    Cpu::op_placeholder, // 0x3B : SRL E
+    Cpu::op_placeholder, // 0x3C : SRL H
+    Cpu::op_placeholder, // 0x3D : SRL L
+    Cpu::op_placeholder, // 0x3E : SRL (HL)
+    Cpu::op_placeholder, // 0x3F : SRL A
+    Cpu::op_placeholder, // 0x40 : BIT 0,B
+    Cpu::op_placeholder, // 0x41 : BIT 0,C
+    Cpu::op_placeholder, // 0x42 : BIT 0,D
+    Cpu::op_placeholder, // 0x43 : BIT 0,E
+    Cpu::op_placeholder, // 0x44 : BIT 0,H
+    Cpu::op_placeholder, // 0x45 : BIT 0,L
+    Cpu::op_placeholder, // 0x46 : BIT 0,(HL)
+    Cpu::op_placeholder, // 0x47 : BIT 0,A
+    Cpu::op_placeholder, // 0x48 : BIT 1,B
+    Cpu::op_placeholder, // 0x49 : BIT 1,C
+    Cpu::op_placeholder, // 0x4A : BIT 1,D
+    Cpu::op_placeholder, // 0x4B : BIT 1,E
+    Cpu::op_placeholder, // 0x4C : BIT 1,H
+    Cpu::op_placeholder, // 0x4D : BIT 1,L
+    Cpu::op_placeholder, // 0x4E : BIT 1,(HL)
+    Cpu::op_placeholder, // 0x4F : BIT 1,A
+    Cpu::op_placeholder, // 0x50 : BIT 2,B
+    Cpu::op_placeholder, // 0x51 : BIT 2,C
+    Cpu::op_placeholder, // 0x52 : BIT 2,D
+    Cpu::op_placeholder, // 0x53 : BIT 2,E
+    Cpu::op_placeholder, // 0x54 : BIT 2,H
+    Cpu::op_placeholder, // 0x55 : BIT 2,L
+    Cpu::op_placeholder, // 0x56 : BIT 2,(HL)
+    Cpu::op_placeholder, // 0x57 : BIT 2,A
+    Cpu::op_placeholder, // 0x58 : BIT 3,B
+    Cpu::op_placeholder, // 0x59 : BIT 3,C
+    Cpu::op_placeholder, // 0x5A : BIT 3,D
+    Cpu::op_placeholder, // 0x5B : BIT 3,E
+    Cpu::op_placeholder, // 0x5C : BIT 3,H
+    Cpu::op_placeholder, // 0x5D : BIT 3,L
+    Cpu::op_placeholder, // 0x5E : BIT 3,(HL)
+    Cpu::op_placeholder, // 0x5F : BIT 3,A
+    Cpu::op_placeholder, // 0x60 : BIT 4,B
+    Cpu::op_placeholder, // 0x61 : BIT 4,C
+    Cpu::op_placeholder, // 0x62 : BIT 4,D
+    Cpu::op_placeholder, // 0x63 : BIT 4,E
+    Cpu::op_placeholder, // 0x64 : BIT 4,H
+    Cpu::op_placeholder, // 0x65 : BIT 4,L
+    Cpu::op_placeholder, // 0x66 : BIT 4,(HL)
+    Cpu::op_placeholder, // 0x67 : BIT 4,A
+    Cpu::op_placeholder, // 0x68 : BIT 5,B
+    Cpu::op_placeholder, // 0x69 : BIT 5,C
+    Cpu::op_placeholder, // 0x6A : BIT 5,D
+    Cpu::op_placeholder, // 0x6B : BIT 5,E
+    Cpu::op_placeholder, // 0x6C : BIT 5,H
+    Cpu::op_placeholder, // 0x6D : BIT 5,L
+    Cpu::op_placeholder, // 0x6E : BIT 5,(HL)
+    Cpu::op_placeholder, // 0x6F : BIT 5,A
+    Cpu::op_placeholder, // 0x70 : BIT 6,B
+    Cpu::op_placeholder, // 0x71 : BIT 6,C
+    Cpu::op_placeholder, // 0x72 : BIT 6,D
+    Cpu::op_placeholder, // 0x73 : BIT 6,E
+    Cpu::op_placeholder, // 0x74 : BIT 6,H
+    Cpu::op_placeholder, // 0x75 : BIT 6,L
+    Cpu::op_placeholder, // 0x76 : BIT 6,(HL)
+    Cpu::op_placeholder, // 0x77 : BIT 6,A
+    Cpu::op_placeholder, // 0x78 : BIT 7,B
+    Cpu::op_placeholder, // 0x79 : BIT 7,C
+    Cpu::op_placeholder, // 0x7A : BIT 7,D
+    Cpu::op_placeholder, // 0x7B : BIT 7,E
+    Cpu::op_placeholder, // 0x7C : BIT 7,H
+    Cpu::op_placeholder, // 0x7D : BIT 7,L
+    Cpu::op_placeholder, // 0x7E : BIT 7,(HL)
+    Cpu::op_placeholder, // 0x7F : BIT 7,A
+    Cpu::op_placeholder, // 0x80 : RES 0,B
+    Cpu::op_placeholder, // 0x81 : RES 0,C
+    Cpu::op_placeholder, // 0x82 : RES 0,D
+    Cpu::op_placeholder, // 0x83 : RES 0,E
+    Cpu::op_placeholder, // 0x84 : RES 0,H
+    Cpu::op_placeholder, // 0x85 : RES 0,L
+    Cpu::op_placeholder, // 0x86 : RES 0,(HL)
+    Cpu::op_placeholder, // 0x87 : RES 0,A
+    Cpu::op_placeholder, // 0x88 : RES 1,B
+    Cpu::op_placeholder, // 0x89 : RES 1,C
+    Cpu::op_placeholder, // 0x8A : RES 1,D
+    Cpu::op_placeholder, // 0x8B : RES 1,E
+    Cpu::op_placeholder, // 0x8C : RES 1,H
+    Cpu::op_placeholder, // 0x8D : RES 1,L
+    Cpu::op_placeholder, // 0x8E : RES 1,(HL)
+    Cpu::op_placeholder, // 0x8F : RES 1,A
+    Cpu::op_placeholder, // 0x90 : RES 2,B
+    Cpu::op_placeholder, // 0x91 : RES 2,C
+    Cpu::op_placeholder, // 0x92 : RES 2,D
+    Cpu::op_placeholder, // 0x93 : RES 2,E
+    Cpu::op_placeholder, // 0x94 : RES 2,H
+    Cpu::op_placeholder, // 0x95 : RES 2,L
+    Cpu::op_placeholder, // 0x96 : RES 2,(HL)
+    Cpu::op_placeholder, // 0x97 : RES 2,A
+    Cpu::op_placeholder, // 0x98 : RES 3,B
+    Cpu::op_placeholder, // 0x99 : RES 3,C
+    Cpu::op_placeholder, // 0x9A : RES 3,D
+    Cpu::op_placeholder, // 0x9B : RES 3,E
+    Cpu::op_placeholder, // 0x9C : RES 3,H
+    Cpu::op_placeholder, // 0x9D : RES 3,L
+    Cpu::op_placeholder, // 0x9E : RES 3,(HL)
+    Cpu::op_placeholder, // 0x9F : RES 3,A
+    Cpu::op_placeholder, // 0xA0 : RES 4,B
+    Cpu::op_placeholder, // 0xA1 : RES 4,C
+    Cpu::op_placeholder, // 0xA2 : RES 4,D
+    Cpu::op_placeholder, // 0xA3 : RES 4,E
+    Cpu::op_placeholder, // 0xA4 : RES 4,H
+    Cpu::op_placeholder, // 0xA5 : RES 4,L
+    Cpu::op_placeholder, // 0xA6 : RES 4,(HL)
+    Cpu::op_placeholder, // 0xA7 : RES 4,A
+    Cpu::op_placeholder, // 0xA8 : RES 5,B
+    Cpu::op_placeholder, // 0xA9 : RES 5,C
+    Cpu::op_placeholder, // 0xAA : RES 5,D
+    Cpu::op_placeholder, // 0xAB : RES 5,E
+    Cpu::op_placeholder, // 0xAC : RES 5,H
+    Cpu::op_placeholder, // 0xAD : RES 5,L
+    Cpu::op_placeholder, // 0xAE : RES 5,(HL)
+    Cpu::op_placeholder, // 0xAF : RES 5,A
+    Cpu::op_placeholder, // 0xB0 : RES 6,B
+    Cpu::op_placeholder, // 0xB1 : RES 6,C
+    Cpu::op_placeholder, // 0xB2 : RES 6,D
+    Cpu::op_placeholder, // 0xB3 : RES 6,E
+    Cpu::op_placeholder, // 0xB4 : RES 6,H
+    Cpu::op_placeholder, // 0xB5 : RES 6,L
+    Cpu::op_placeholder, // 0xB6 : RES 6,(HL)
+    Cpu::op_placeholder, // 0xB7 : RES 6,A
+    Cpu::op_placeholder, // 0xB8 : RES 7,B
+    Cpu::op_placeholder, // 0xB9 : RES 7,C
+    Cpu::op_placeholder, // 0xBA : RES 7,D
+    Cpu::op_placeholder, // 0xBB : RES 7,E
+    Cpu::op_placeholder, // 0xBC : RES 7,H
+    Cpu::op_placeholder, // 0xBD : RES 7,L
+    Cpu::op_placeholder, // 0xBE : RES 7,(HL)
+    Cpu::op_placeholder, // 0xBF : RES 7,A
+    Cpu::op_placeholder, // 0xC0 : SET 0,B
+    Cpu::op_placeholder, // 0xC1 : SET 0,C
+    Cpu::op_placeholder, // 0xC2 : SET 0,D
+    Cpu::op_placeholder, // 0xC3 : SET 0,E
+    Cpu::op_placeholder, // 0xC4 : SET 0,H
+    Cpu::op_placeholder, // 0xC5 : SET 0,L
+    Cpu::op_placeholder, // 0xC6 : SET 0,(HL)
+    Cpu::op_placeholder, // 0xC7 : SET 0,A
+    Cpu::op_placeholder, // 0xC8 : SET 1,B
+    Cpu::op_placeholder, // 0xC9 : SET 1,C
+    Cpu::op_placeholder, // 0xCA : SET 1,D
+    Cpu::op_placeholder, // 0xCB : SET 1,E
+    Cpu::op_placeholder, // 0xCC : SET 1,H
+    Cpu::op_placeholder, // 0xCD : SET 1,L
+    Cpu::op_placeholder, // 0xCE : SET 1,(HL)
+    Cpu::op_placeholder, // 0xCF : SET 1,A
+    Cpu::op_placeholder, // 0xD0 : SET 2,B
+    Cpu::op_placeholder, // 0xD1 : SET 2,C
+    Cpu::op_placeholder, // 0xD2 : SET 2,D
+    Cpu::op_placeholder, // 0xD3 : SET 2,E
+    Cpu::op_placeholder, // 0xD4 : SET 2,H
+    Cpu::op_placeholder, // 0xD5 : SET 2,L
+    Cpu::op_placeholder, // 0xD6 : SET 2,(HL)
+    Cpu::op_placeholder, // 0xD7 : SET 2,A
+    Cpu::op_placeholder, // 0xD8 : SET 3,B
+    Cpu::op_placeholder, // 0xD9 : SET 3,C
+    Cpu::op_placeholder, // 0xDA : SET 3,D
+    Cpu::op_placeholder, // 0xDB : SET 3,E
+    Cpu::op_placeholder, // 0xDC : SET 3,H
+    Cpu::op_placeholder, // 0xDD : SET 3,L
+    Cpu::op_placeholder, // 0xDE : SET 3,(HL)
+    Cpu::op_placeholder, // 0xDF : SET 3,A
+    Cpu::op_placeholder, // 0xE0 : SET 4,B
+    Cpu::op_placeholder, // 0xE1 : SET 4,C
+    Cpu::op_placeholder, // 0xE2 : SET 4,D
+    Cpu::op_placeholder, // 0xE3 : SET 4,E
+    Cpu::op_placeholder, // 0xE4 : SET 4,H
+    Cpu::op_placeholder, // 0xE5 : SET 4,L
+    Cpu::op_placeholder, // 0xE6 : SET 4,(HL)
+    Cpu::op_placeholder, // 0xE7 : SET 4,A
+    Cpu::op_placeholder, // 0xE8 : SET 5,B
+    Cpu::op_placeholder, // 0xE9 : SET 5,C
+    Cpu::op_placeholder, // 0xEA : SET 5,D
+    Cpu::op_placeholder, // 0xEB : SET 5,E
+    Cpu::op_placeholder, // 0xEC : SET 5,H
+    Cpu::op_placeholder, // 0xED : SET 5,L
+    Cpu::op_placeholder, // 0xEE : SET 5,(HL)
+    Cpu::op_placeholder, // 0xEF : SET 5,A
+    Cpu::op_placeholder, // 0xF0 : SET 6,B
+    Cpu::op_placeholder, // 0xF1 : SET 6,C
+    Cpu::op_placeholder, // 0xF2 : SET 6,D
+    Cpu::op_placeholder, // 0xF3 : SET 6,E
+    Cpu::op_placeholder, // 0xF4 : SET 6,H
+    Cpu::op_placeholder, // 0xF5 : SET 6,L
+    Cpu::op_placeholder, // 0xF6 : SET 6,(HL)
+    Cpu::op_placeholder, // 0xF7 : SET 6,A
+    Cpu::op_placeholder, // 0xF8 : SET 7,B
+    Cpu::op_placeholder, // 0xF9 : SET 7,C
+    Cpu::op_placeholder, // 0xFA : SET 7,D
+    Cpu::op_placeholder, // 0xFB : SET 7,E
+    Cpu::op_placeholder, // 0xFC : SET 7,H
+    Cpu::op_placeholder, // 0xFD : SET 7,L
+    Cpu::op_placeholder, // 0xFE : SET 7,(HL)
+    Cpu::op_placeholder, // 0xFF : SET 7,A
+];
+
 const STATUS_FLAG_Z: u8 = 0x80;
 const STATUS_FLAG_N: u8 = 0x40;
 const STATUS_FLAG_H: u8 = 0x20;
@@ -340,6 +599,8 @@ impl Cpu {
 
     pub fn tick(&mut self) {
         OP_CODE_FUNCTION_TABLE[self.fetch_u8() as usize](self);
+        // TODO to remove
+        CB_CODE_FUNCTION_TABLE[self.fetch_u8() as usize](self);
     }
 
     fn read_hl(&mut self) -> u8 {
@@ -575,6 +836,23 @@ impl Cpu {
 
         if (result & 0xF) == 0 {
             self.status_flags |= STATUS_FLAG_H;
+        }
+
+        result
+    }
+
+    fn run_rlc_u8_and_update_flags(&mut self, operand: u8) -> u8 {
+        let carry = (operand & 0x80) != 0;
+        let result = operand.wrapping_shl(1) | carry as u8;
+
+        self.status_flags = 0;
+
+        if carry {
+            self.status_flags |= STATUS_FLAG_C;
+        }
+
+        if operand == 0 {
+            self.status_flags |= STATUS_FLAG_Z;
         }
 
         result
@@ -2728,5 +3006,66 @@ impl Cpu {
     fn op_rst_38h(&mut self) {
         self.stack_push_u16(self.program_counter);
         self.program_counter = 0x0038;
+    }
+}
+
+impl Cpu {
+    /// Opcode 0x00: [RLC B](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register B (2 machine cycles).
+    fn cb_rlc_b(&mut self) {
+        self.registers.register_b = self.run_rlc_u8_and_update_flags(self.registers.register_b);
+    }
+
+    /// Opcode 0x01: [RLC C](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register C (2 machine cycles).
+    fn cb_rlc_c(&mut self) {
+        self.registers.register_c = self.run_rlc_u8_and_update_flags(self.registers.register_c);
+    }
+
+    /// Opcode 0x02: [RLC D](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register D (2 machine cycles).
+    fn cb_rlc_d(&mut self) {
+        self.registers.register_d = self.run_rlc_u8_and_update_flags(self.registers.register_d);
+    }
+
+    /// Opcode 0x03: [RLC E](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register E (2 machine cycles).
+    fn cb_rlc_e(&mut self) {
+        self.registers.register_e = self.run_rlc_u8_and_update_flags(self.registers.register_e);
+    }
+
+    /// Opcode 0x04: [RLC H](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register H (2 machine cycles).
+    fn cb_rlc_h(&mut self) {
+        self.registers.register_h = self.run_rlc_u8_and_update_flags(self.registers.register_h);
+    }
+
+    /// Opcode 0x05: [RLC L](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register L (2 machine cycles).
+    fn cb_rlc_l(&mut self) {
+        self.registers.register_l = self.run_rlc_u8_and_update_flags(self.registers.register_l);
+    }
+
+    /// Opcode 0x06: [RLC (HL)](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of data from the absolute address specified by the 16-bit
+    /// register HL (4 machine cycles).
+    fn cb_rlc_hl(&mut self) {
+        let operand = self.read_hl();
+        let value = self.run_rlc_u8_and_update_flags(operand);
+        self.write_hl(value);
+    }
+
+    /// Opcode 0x07: [RLC A](https://gekkio.fi/files/gb-docs/gbctr.pdf#page=81)
+    ///
+    /// Circular rotate left of the 8-bit register A (2 machine cycles).
+    fn cb_rlc_a(&mut self) {
+        self.registers.register_a = self.run_rlc_u8_and_update_flags(self.registers.register_a);
     }
 }
