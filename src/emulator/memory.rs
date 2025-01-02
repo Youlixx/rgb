77295f83c55d19f1e0c5395c146c6caa285c0a13
Youlixx <combey.theo@hotmail.com>
2025-01-02 20:14:17 +0000

    Reworked the memory map

--- src/emulator/memory.rs
+++ src/emulator/memory.rs
@@ -1,94 +1,107 @@
-use super::{
-    interrupts::{self, Interrupts},
-    timer::{self, ConsoleTimer, Timer},
-};
+use super::interrupts::{Interrupt, InterruptRegisters};
 
+/// Memory trait.
 pub trait Memory {
-    fn silent_read(&self, address: usize) -> u8;
-    fn silent_write(&mut self, address: usize, value: u8);
+    /// Read a value from the component memory. The address is always given in
+    /// the absolute address space of the emulator. If the address is out of
+    /// bound of the component memory, the function should return None.
+    fn read(&self, address: usize) -> Option<u8>;
+
+    /// Write a value to the component memory. The address is always given in
+    /// the absolute address space of the emulator. If the address is out of
+    /// bound of the component memory, the function should return None.
+    fn write(&mut self, address: usize, value: u8) -> bool;
 }
 
-pub trait Tickable<T> {
-    fn tick(&mut self) -> T;
+/// Component trait.
+pub trait Component: Memory {
+    /// Tick the component. May return an interrupt to communicate to the CPU.
+    fn tick(&mut self) -> Option<Interrupt>;
 }
 
-struct DefaultSerialPort {}
-
-impl Memory for DefaultSerialPort {
-    fn silent_read(&self, _: usize) -> u8 {
-        0
+/// Inhert memories can be tickable, but they never issue any interrupt.
+impl<T> Component for T
+where
+    T: Memory,
+{
+    /// Since the component is a simple memory, it never issue any interrupt.
+    fn tick(&mut self) -> Option<Interrupt> {
+        None
     }
-
-    fn silent_write(&mut self, _: usize, _: u8) {}
 }
 
-pub struct ConsoleMemory {
-    interrupts: Interrupts,
+/// Console memory map.
+pub struct MemoryMap {
+    /// Console components (e.g. memory, PPU, APU, ...)
+    components: Vec<Box<dyn Component>>,
 
-    memory: Vec<u8>, // TODO: temporary, not everything needs to be mapped... + mirroring
-    timer: Box<dyn Timer>,
-    serial_port: Box<dyn Memory>,
+    /// Console interrupt registers, always present.
+    /// The components should never overlap with $0xFF0F and $0xFFFF or the
+    /// interrupt will not be triggered.
+    pub interrupt_registers: InterruptRegisters,
 }
 
-impl ConsoleMemory {
-    pub fn new(
-        rom: &[u8],
-        timer: Option<Box<dyn Timer>>,
-        serial_port: Option<Box<dyn Memory>>,
-    ) -> Self {
-        let mut memory = vec![0; 0x10000];
-        memory[..rom.len()].copy_from_slice(rom);
-
+impl MemoryMap {
+    pub fn new(components: Vec<Box<dyn Component>>) -> Self {
         Self {
-            memory,
-            interrupts: Interrupts::new(),
-            timer: timer.unwrap_or(Box::new(ConsoleTimer::new())),
-            serial_port: serial_port.unwrap_or(Box::new(DefaultSerialPort {})),
+            components,
+            interrupt_registers: InterruptRegisters::new(),
         }
     }
 
+    /// Tick the component and issue interrupts.
     pub fn tick(&mut self) {
-        self.interrupts.update_flags(self.timer.tick());
+        self.components
+            .iter_mut()
+            .filter_map(|component| component.tick())
+            .for_each(|interrupt| self.interrupt_registers.update_flags(interrupt));
     }
 
+    /// Perform a 'silent' read. Directly read to the memory without ticking
+    /// the internals, this function is exposed for debug purposes, but may
+    /// corrupt the emulation state. If the address is not mapped by any
+    /// component, this function will panic.
+    pub fn read(&self, address: usize) -> u8 {
+        self.components
+            .iter()
+            .find_map(|memory| memory.read(address))
+            .or_else(|| self.interrupt_registers.read(address))
+            .expect("Tried to read from an unmapped area of the memory.")
+    }
+
+    /// Perform a 'silent' write. Directly write to the memory without ticking
+    /// the internals, this function is exposed for debug purposes, but may
+    /// corrupt the emulation state. If the address is not mapped by any
+    /// component, this function will panic.
+    pub fn write(&mut self, address: usize, value: u8) {
+        if !(self
+            .components
+            .iter_mut()
+            .any(|memory| memory.write(address, value))
+            || self.interrupt_registers.write(address, value))
+        {
+            panic!("Tried to write to an unmapped area of the memory.")
+        }
+    }
+
+    /// Perform a read operation and tick the internals. If the address is not
+    /// mapped by any component, this function will panic.
     pub fn cycle_read(&mut self, address: usize) -> u8 {
         self.tick();
-        self.silent_read(address)
+        self.read(address)
     }
 
+    /// Perform a write operation and tick the internals. If the address is not
+    /// mapped by any component, this function will panic.
     pub fn cycle_write(&mut self, address: usize, value: u8) {
         self.tick();
-        self.silent_write(address, value);
-    }
-
-    pub fn should_interrupt(&self) -> bool {
-        self.interrupts.should_interrupt()
-    }
-
-    pub fn get_interrupt_address(&mut self) -> Option<usize> {
-        self.interrupts.get_interrupt_address()
+        self.write(address, value);
     }
 }
 
-// TODO branching for IO registers, maybe separate struct for em
-impl Memory for ConsoleMemory {
-    fn silent_read(&self, address: usize) -> u8 {
-        match address {
-            0xFF01..=0xFF02 => self.serial_port.silent_read(address),
-            timer::address::DIV..=timer::address::TAC => self.timer.silent_read(address),
-            interrupts::address::INTERRUPTS_FLAGS => self.interrupts.read_flags(),
-            interrupts::address::INTERRUPTS_ENABLE => self.interrupts.read_enable(),
-            address => self.memory[address],
-        }
-    }
-
-    fn silent_write(&mut self, address: usize, value: u8) {
-        match address {
-            0xFF01..=0xFF02 => self.serial_port.silent_write(address, value),
-            timer::address::DIV..=timer::address::TAC => self.timer.silent_write(address, value),
-            interrupts::address::INTERRUPTS_FLAGS => self.interrupts.write_flags(value),
-            interrupts::address::INTERRUPTS_ENABLE => self.interrupts.write_enable(value),
-            address => self.memory[address] = value,
-        };
+impl Default for MemoryMap {
+    /// Returns the "default value" for a type.
+    fn default() -> Self {
+        Self::new(vec![])
     }
 }
